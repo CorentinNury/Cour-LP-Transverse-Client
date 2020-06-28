fc466a1b24790a099410e7e48ecc513fb8cce9ef
Dreymst <deat.vincent@gmail.com>
2020-06-28 23:20:59 +0200

    navbar + readme

--- README.md
+++ README.md
@@ -1,53 +1,5 @@
-# Cour-LP-React-PWA
+Pour lancer le projet, il suffit de fair npm install puis un npm start.
 
-Going to create a React App
-Build a PWA installable APP
-With a little design framework 
-
-## First
-
-Via npm, we gonna clone the Create-React-App template purposed by Facebook. 
-
-### npx
-
-```sh
-npx create-react-app my-app
-```
-
-### or via npm
-
-```sh
-npm init react-app my-app
-```
-
-## Second 
-
-Now we have the app ! Cool, 
-What you can do for now: 
-
-Success! Created my-app at C:\Users\ppier\Developpement\Cour-LP-React-PWA\my-app
-Inside that directory, you can run several commands:
-
-```sh
-  npm start
-``` 
-    Starts the development server.
-```sh
-  npm run build
-```
-    Bundles the app into static files for production.
-```sh
-  npm test
-```
-    Starts the test runner.
-```sh
-  npm run eject
-```
-    Removes this tool and copies build dependencies, configuration files
-    and scripts into the app directory. If you do this, you can’t go back!
-
-We suggest that you begin by typing:
-```sh
-  cd my-app
-  npm start
-```
+Nous somme partis du projet de base.
+Il est possible de créer des "projects", de créer des "tasks" et ensuite d'ajouter des tasks à des projects.
+Il est aussi possible de se créer un compte utilisateur et de s'y connecter.
--- my-app/src/component/common/Navbar.js
+++ my-app/src/component/common/Navbar.js
@@ -9,7 +9,7 @@ class Navbar extends Component {
   render() 
   {
     return(
-      <nav className="navbar navbar-expand-lg navbar-light ">
+      <nav className="navbar navbar-expand-lg navbar-light " >
         <img src={logo} className="app-logo" alt="logo" />
         <a className="navbar-brand" href="/home">Only fan</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,18 +17,10 @@ class Navbar extends Component {
         </button>        
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/home">Home <span className="sr-only">(current)</span></Link>
-            </li>      
-            <li className="nav-item">
-              <Link className="nav-link" to="/new-user">Register</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/projects">Projects</Link>
-            </li>
+            <li className="nav-item active"><Link className="nav-link" to="/home">Home <span className="sr-only">(current)</span></Link></li>      
+            <li className="nav-item"><Link className="nav-link" to="/new-user">Register</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/projects">Projects</Link></li>
           </ul>
         </div>
       </nav>
